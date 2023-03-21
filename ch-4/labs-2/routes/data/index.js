1f7cdd8d1cdca4f5dcf5e06e5284d686524a2088
AdBirecki <abirecki@gmail.com>
2023-03-21 18:37:27 +0100

    CH-4 labs

---
+++ ch-4/labs-2/routes/data/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const data = require('../../stream')
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    return data();
+  })
+}
